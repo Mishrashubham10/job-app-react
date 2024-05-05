7ee62d723ec7790100c7793106d5663c16acf5d3
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-05-05 23:24:27 +0530

    Creates essential components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-const App = () => {
-  return (
-    <div className="text-6xl text-red-500">
-      <h1>Job App with React Vite.</h1>
-    </div>
-  )
-}
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
-export default App
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import ViewAllJobs from './components/ViewAllJobs';
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+      <Route path="/" element={<HomeCards />} />
+      <Route path="/hero" element={<Hero />} />
+      <Route path="/view-jobs" element={<ViewAllJobs />} />
+      </>
+    )
+  );
+
+  return (
+    <RouterProvider router={router} />
+  );
+};
+
+export default App;
