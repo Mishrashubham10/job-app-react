3d3d9608581b427d76a86ee09b155f0284bba229
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-05-06 11:19:48 +0530

    Creates loading spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,31 +1,48 @@
-import JobListing from "./JobListing"
-import jobs from "../jobs.json"
-import { useState } from "react";
+/* eslint-disable react/prop-types */
+import JobListing from './JobListing';
+import { useState, useEffect } from 'react';
+import Spinner from './Spinner';
 
-const JobListings = () => {
-    const [loading, setLoading] = useState(false)
-    console.log(jobs, setLoading);
+const JobListings = ({ isHome = false }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setData(data);
+      } catch (err) {
+        console.log('Error fetching data', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
-    <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-          {/* {isHome ? 'Recent Jobs' : 'Browse Jobs'} */}
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
           Browse Jobs
         </h2>
 
         {loading ? (
-          <p>Loading...</p>
+          <Spinner loading={loading} />
         ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map((job) => (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {data.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
           </div>
         )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default JobListings
+export default JobListings;
